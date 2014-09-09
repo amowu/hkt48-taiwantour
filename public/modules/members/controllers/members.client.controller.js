a13c3c2ea3697ae4fe84e8d2472df37cb397631b
Amo Wu <amo260@gmail.com>
2014-09-09 21:11:18 +0800

    Replace tab to space

--- public/modules/members/controllers/members.client.controller.js
+++ public/modules/members/controllers/members.client.controller.js
@@ -2,64 +2,64 @@
 
 // Members controller
 angular.module('members').controller('MembersController', ['$scope', '$stateParams', '$location', 'Authentication', 'Members',
-	function($scope, $stateParams, $location, Authentication, Members ) {
-		$scope.authentication = Authentication;
+  function($scope, $stateParams, $location, Authentication, Members ) {
+    $scope.authentication = Authentication;
 
-		// Create new Member
-		$scope.create = function() {
-			// Create new Member object
-			var member = new Members ({
-				name: this.name
-			});
+    // Create new Member
+    $scope.create = function() {
+      // Create new Member object
+      var member = new Members ({
+        name: this.name
+      });
 
-			// Redirect after save
-			member.$save(function(response) {
-				$location.path('members/' + response._id);
+      // Redirect after save
+      member.$save(function(response) {
+        $location.path('members/' + response._id);
 
-				// Clear form fields
-				$scope.name = '';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+        // Clear form fields
+        $scope.name = '';
+      }, function(errorResponse) {
+        $scope.error = errorResponse.data.message;
+      });
+    };
 
-		// Remove existing Member
-		$scope.remove = function( member ) {
-			if ( member ) { member.$remove();
+    // Remove existing Member
+    $scope.remove = function( member ) {
+      if ( member ) { member.$remove();
 
-				for (var i in $scope.members ) {
-					if ($scope.members [i] === member ) {
-						$scope.members.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.member.$remove(function() {
-					$location.path('members');
-				});
-			}
-		};
+        for (var i in $scope.members ) {
+          if ($scope.members [i] === member ) {
+            $scope.members.splice(i, 1);
+          }
+        }
+      } else {
+        $scope.member.$remove(function() {
+          $location.path('members');
+        });
+      }
+    };
 
-		// Update existing Member
-		$scope.update = function() {
-			var member = $scope.member ;
+    // Update existing Member
+    $scope.update = function() {
+      var member = $scope.member ;
 
-			member.$update(function() {
-				$location.path('members/' + member._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+      member.$update(function() {
+        $location.path('members/' + member._id);
+      }, function(errorResponse) {
+        $scope.error = errorResponse.data.message;
+      });
+    };
 
-		// Find a list of Members
-		$scope.find = function() {
-			$scope.members = Members.query();
-		};
+    // Find a list of Members
+    $scope.find = function() {
+      $scope.members = Members.query();
+    };
 
-		// Find existing Member
-		$scope.findOne = function() {
-			$scope.member = Members.get({ 
-				memberId: $stateParams.memberId
-			});
-		};
-	}
+    // Find existing Member
+    $scope.findOne = function() {
+      $scope.member = Members.get({
+        memberId: $stateParams.memberId
+      });
+    };
+  }
 ]);
