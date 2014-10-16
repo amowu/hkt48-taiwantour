24dfaec55bc43ebfe68ad4737b117b324fce3937
Amo Wu <amo260@gmail.com>
2014-10-16 14:55:57 +0800

    Update member view page image load.

--- public/modules/core/directives/imageload.client.directive.js
+++ public/modules/core/directives/imageload.client.directive.js
@@ -1,12 +1,11 @@
 'use strict';
 
-angular.module('core').directive('imageloaded', [
+angular.module('core').directive('imageload', [
   function() {
     return {
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
         var cssClass = attrs.loadedclass;
-
         element.bind('load', function(e) {
           angular.element(element).addClass(cssClass);
         });
