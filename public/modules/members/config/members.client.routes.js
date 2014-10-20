bc7f669e87b7f27514457cc428b9484c6dce4028
Amo Wu <amo260@gmail.com>
2014-10-20 19:59:50 +0800

    Remove unused routes.

--- public/modules/members/config/members.client.routes.js
+++ public/modules/members/config/members.client.routes.js
@@ -9,17 +9,9 @@ angular.module('members').config(['$stateProvider',
 			url: '/members',
 			templateUrl: 'modules/members/views/list-members.client.view.html'
 		}).
-		state('createMember', {
-			url: '/members/create',
-			templateUrl: 'modules/members/views/create-member.client.view.html'
-		}).
 		state('viewMember', {
 			url: '/members/:memberFullName',
 			templateUrl: 'modules/members/views/view-member.client.view.html'
-		}).
-		state('editMember', {
-			url: '/members/:memberId/edit',
-			templateUrl: 'modules/members/views/edit-member.client.view.html'
 		});
 	}
 ]);
