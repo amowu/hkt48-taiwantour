87b183e09190d03134ce990ef7e3202127f5d6df
Amo Wu <amo260@gmail.com>
2014-10-20 20:01:17 +0800

    Code refactor.

--- public/modules/members/controllers/members.client.controller.js
+++ public/modules/members/controllers/members.client.controller.js
@@ -5,51 +5,82 @@ angular.module('members').controller('MembersController', ['$scope', '$filter', 
   function($scope, $filter, $http, $stateParams, $location, Authentication) {
     $scope.authentication = Authentication;
 
-    // Find a list of Members
+    // Find a list of Members.
     $scope.find = function() {
       $http.get('modules/members/members.json').success(function(data) {
         $scope.members = data;
       });
     };
 
-    // Find existing Member
+    // Find existing Member.
     $scope.findOne = function() {
-      $http.get('modules/members/members.json').success(function(members) {
-        // find member object by full name
+      
+      // Initialize.
+      $scope.members = null; // All members data.
+      $scope.keywords = null; // Current member's keywords array.
+      $scope.relationships = null; // Current member's relationships array.
+      $scope.details = null; // Current member's relationship details array.
+      $scope.d3Nodes = null; // Current member's relationship nodes array for d3.
+      $scope.d3Links = null; // Current member's relationship links array for d3.
+      $scope.currentGroup = 0; // Current selected member's relationship detail group number.
+      $scope.svgWidth = 0; // d3 svg canvas width.
+      $scope.svgHeight = 0; // d3 svg canvas height.
+      $scope.onTargetMemberHover = null; // Current member's target relationship member mouseover event.
+      $scope.onTargetMemberClick = null; // Current member's target relationship member mouseclick event.
+      $scope.images = null; // Currnt member's images array.
+
+      // Get all members data.
+      //
+      // Why need to get all members data?
+      // Because URL route use member fullname not member id,
+      // so I need to get all members data for find member object use.
+      $http.get('modules/members/members.json').error(function(data, status, headers, config) {
+        // Called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log('HTTP get members.json error.');
+        console.log(data);
+      }).success(function(members) {
+        // Find member object by fullname.
         var member = $filter('getMemberIdByFullName')(members, $stateParams.memberFullName);
-        // use member ID to get tabletop data
+        // Use tabletop to get member's data on google spreadsheet.
         Tabletop.init({
-          key: '1MlbQ8V-O2Q3vGmquuzqUV4whqtlTpzkKF6VFNTgsYx4',
-          query: 'memberid = ' + member.memberid,
+          key: '1MlbQ8V-O2Q3vGmquuzqUV4whqtlTpzkKF6VFNTgsYx4', // Key for google spreadsheet.
+          query: 'memberid = ' + member.memberid, // Query by Member ID cloumn.
           callback: function(data, tabletop) {
-            // console.log(tabletop.sheets('member').all());
-            // console.log(tabletop.sheets('keyword').all());
-            // console.log(tabletop.sheets('relationship').all());
-            // console.log(tabletop.sheets('image').all());
+            // Callback data will return four array:
+            // 1. Current member array = tabletop.sheets('member')
+            // 2. Current member's keywords array = tabletop.sheets('keyword')
+            // 3. Current member's relationships array = tabletop.sheets('relationship')
+            // 4. Current member's images array = tabletop.sheets('image')
             $scope.$apply(function() {
               $scope.member = tabletop.sheets('member').all()[0];
               $scope.keywords = tabletop.sheets('keyword').all();
               $scope.relationships = tabletop.sheets('relationship').all();
               $scope.images = $filter('shuffle')(tabletop.sheets('image').all());
-              // TODO: d3 test code
+              // Initialize D3 module.
               var forceData = $filter('getForceData')($scope.relationships, members, $scope.member.memberid);
+              $scope.d3Nodes = forceData.nodes;
+              $scope.d3Links = forceData.links;
               $scope.details = forceData.details;
-              $scope.nodes = forceData.nodes;
-              $scope.links = forceData.links;
-              $scope.width = 800;
-              $scope.height = 600;
               $scope.currentGroup = 0;
+              $scope.svgWidth = 800;
+              $scope.svgHeight = 600;
               $scope.onTargetMemberHover = function($event) {
                 var targetMemberId = angular.element($event.target).scope().node.memberid;
-                var group = $filter('getGroupByTartgetMemberId')($scope.links, targetMemberId);
+                var group = $filter('getGroupByTartgetMemberId')($scope.d3Links, targetMemberId);
                 $scope.currentGroup = group;
               };
+              $scope.onTargetMemberClick = function($event) {
+                var member = angular.element($event.target).scope().node;
+                var fullName = member.firstnameen + '-' + member.lastnameen;
+                $location.path('members/' + fullName);
+              };
               var force = d3.layout.force()
-                .nodes($scope.nodes)
-                .links($scope.links)
+                .nodes($scope.d3Nodes)
+                .links($scope.d3Links)
                 .charge(-1000)
                 .linkDistance(200)
-                .size([$scope.width, $scope.height])
+                .size([$scope.svgWidth, $scope.svgHeight])
                 .on('tick', function() {
                   $scope.$apply();
                 })
@@ -57,10 +88,6 @@ angular.module('members').controller('MembersController', ['$scope', '$filter', 
             });
           }
         });
-      }).error(function(data, status, headers, config) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        console.log(status);
       });
     };
   }
